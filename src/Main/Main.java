6f4893bd28476858338958460bdb94a3018cdcdf
shakeRocketAndRoll <57106379+shakeRocketAndRoll@users.noreply.github.com>
2020-12-04 16:08:41 +0400

    Menu Added

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -1,59 +1,93 @@
 package Main;
 
-import Interfaces.I_File;
-import Interfaces.I_Video;
-import Media_SubClasses.*;
-import Model.File;
-import Text_SubClasses.*;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Audio audio1 = new Audio("20 Nov 2020", "20 Nov 2020", "mp3", "audio1",
-                444, "Gharibyan Sh.", 44, false, false,
-                "some tracks");
-        audio1.printInfo();
-        audio1.recordAudio();
-        audio1.setVoice_volume(66);
-        System.out.println(audio1.getVoice_volume());
-        audio1.stop();
-        System.out.println(audio1.volumeUp());
-        System.out.println("***************************");
-
-        PDF pdf1 = new PDF();
-        pdf1.setFileFormat("pdf");
-        pdf1.setFileName("SmithSmith");
-        pdf1.setNumberOfPages(4);
-        pdf1.setSecured(true);
-        pdf1.printInfo();
-        System.out.println("***************************");
-
-        Video video1 = new Video("20 Nov 2020", "20 Nov 2020", "mp4", "video1",
-                12560, "Jack Daniels", 3600, false, true,
-                "Avatar, Great Gatsby", "", "", "1,280×720", "Fantasy");
-        video1.printInfo();
-        System.out.println(video1.volumeDown());
-        video1.play();
-        video1.zoomIn();
-        video1.zoomOut();
-        video1.changeQuality("1920x720");
-        video1.turnOnCaptions();
-        video1.turnOffCaptions();
-        System.out.println("***************************");
-
-        PPT ppt1 = new PPT("", "", "ppt", "ppt1", 128, "Sam Smith",
-                12, true);
-        ppt1.printInfo();
-        System.out.println("***************************");
-        ppt1.printAuthor();
-        ppt1.setAuthor("Guuuan hisdl");
-        ppt1.printAuthor();
-        ppt1.setCreationDate("20 Nov 2020");
-        printFile(video1);
+        executeMainMenu();
     }
 
-    public static void printFile(I_File f) {
-        f.printAuthor();
+    static void executeMainMenu() {
+        Scanner scan = new Scanner(System.in);
+        boolean isActive = true;
+        while (isActive) {
+            System.out.println("----------Menu-----------");
+            System.out.println("1. Choose PDF SubMenu");
+            System.out.println("2. Choose Audio SubMenu");
+            System.out.println("3. Exit the Menu!");
+
+            int command = scan.nextInt();
+            switch (command) {
+                case 1:
+                    executePDFSubMenu();
+                    break;
+                case 2:
+                    executeAudioSubMenu();
+                    break;
+                case 3:
+                    System.out.println("Have a nice day.");
+                    isActive = false;
+                    break;
+                default:
+                    System.out.println("Invalid command");
+                    break;
+            }
+        }
     }
 
+    static void executePDFSubMenu() {
+        Scanner scan = new Scanner(System.in);
+        boolean isActive = true;
+        while (isActive) {
+            System.out.println("------------PDF SubMenu--------------");
+            System.out.println("1. Create PDF File");
+            System.out.println("2. Print all PDF files");
+            System.out.println("3. Exit the PDF SubMenu");
+            int command = scan.nextInt();
+            switch (command) {
+                case 1:
+                    System.out.println("Creating PDF files");
+                    //Services.PDF_Service.createPDFFile();
+                    break;
+                case 2:
+                    System.out.println("Printing all PDF files");
+                    break;
+                case 3:
+                    System.out.println("Exit SubMenu");
+                    isActive = false;
+                    break;
+                default:
+                    System.out.println("Invalid command");
+                    break;
+            }
+        }
+    }
+
+    static void executeAudioSubMenu() {
+        Scanner scan = new Scanner(System.in);
+        boolean isActive = true;
+        while (isActive) {
+            System.out.println("------------Audio SubMenu--------------");
+            System.out.println("1. Create Audio File");
+            System.out.println("2. Print all Audio files");
+            System.out.println("3. Exit the Audio SubMenu");
+            int command = scan.nextInt();
+            switch (command) {
+                case 1:
+                    System.out.println("Creating Audio File");
+                    break;
+                case 2:
+                    System.out.println("Printing all Audio files");
+                    break;
+                case 3:
+                    System.out.println("Exit SubMenu");
+                    isActive = false;
+                    break;
+                default:
+                    System.out.println("Invalid command");
+                    break;
+            }
+        }
+    }
 }
 
