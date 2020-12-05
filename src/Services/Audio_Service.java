0fe8fe65dd604bc98b4578a77dca72bc7c76c009
shakeRocketAndRoll <57106379+shakeRocketAndRoll@users.noreply.github.com>
2020-12-06 03:15:28 +0400

    Fixed Audio_Service

--- src/Services/Audio_Service.java
+++ src/Services/Audio_Service.java
@@ -3,10 +3,11 @@ package Services;
 import Media_SubClasses.Audio;
 import Text_SubClasses.PDF;
 
+import java.io.IOException;
 import java.util.Scanner;
 
 public class Audio_Service {
-    private static String path = "ForAudioObjects.txt";
+    private static final String PATH = "ForAudioObjects.txt";
 
     public static Audio createAudioFile() throws Exception {
         Scanner scan = new Scanner(System.in);
@@ -22,41 +23,51 @@ public class Audio_Service {
         audio.setDurationOfTrack(scan.nextInt());
         System.out.println("Set mode for record status: 1. Licensed , 2. Not Licensed.");
         audio.setLicensedRecord(scan.nextInt() == 1);
-        File_Service.write(path, audio);
-        System.out.println("***************************");
+        try {
+            File_Service.write(PATH, audio);
+        } catch (IOException e){
+            System.out.println("Could not write to the file.");
+        }        System.out.println("***************************");
         return audio;
     }
 
-    public static void printAudioFiles() throws Exception {
-        String[] audios = File_Service.read(path);
-        if (File_Service.checkArrayIsEmpty(audios)) {
-            return;
+    private static Audio[] readAudioFiles() {
+        String[] strings = {};
+        try {
+            strings = File_Service.read(PATH);
+        } catch (IOException e) {
+            System.out.println("Could not read Audio files");
         }
-        for (String a : audios) {
-            Audio audio = new Audio(a);
-            audio.printInfo();
+        Audio[] audios = new Audio[strings.length];
+        for (int i = 0; i < audios.length; i++) {
+            try {
+                audios[i] = new Audio(strings[i]);
+            } catch (IndexOutOfBoundsException e) {
+                System.out.println("Could not read PDF files due to corrupted record");
+                return new Audio[]{};
+            }
+        }
+        return audios;
+    }
+
+    public static void printAudioFiles() throws Exception {
+        for (Audio a : readAudioFiles()) {
+            a.printInfo();
         }
     }
 
     public static void sortByTrackDuration() throws Exception {
-        String[] audios = File_Service.read(path);
-        if (File_Service.checkArrayIsEmpty(audios)) {
-            return;
-        }
-        Audio[] real_Audios = new Audio[audios.length];
-        for (int i = 0; i < real_Audios.length; i++) {
-            real_Audios[i] = new Audio(audios[i]);
-        }
-        for (int i = 0; i < real_Audios.length; i++) {
+        Audio[] audios = readAudioFiles();
+        for (int i = 0; i < audios.length; i++) {
             for (int j = 0; j < audios.length - 1 - i; j++) {
-                if (real_Audios[j].getDurationOfTrack() > real_Audios[j + 1].getDurationOfTrack()) {
-                    Audio reservedAudio = real_Audios[j];
-                    real_Audios[j] = real_Audios[j + 1];
-                    real_Audios[j + 1] = reservedAudio;
+                if (audios[j].getDurationOfTrack() > audios[j + 1].getDurationOfTrack()) {
+                    Audio reservedAudio = audios[j];
+                    audios[j] = audios[j + 1];
+                    audios[j + 1] = reservedAudio;
                 }
             }
         }
-        for (Audio a : real_Audios) {
+        for (Audio a : audios) {
             a.printInfo();
         }
     }
