38039beae8f5fb7d8291f25375fb4ebaf4a6bc9a
shakeRocketAndRoll <57106379+shakeRocketAndRoll@users.noreply.github.com>
2021-01-06 22:01:27 +0200

    fixed line 8 in video submenu

--- src/Main/FilesMenus.java
+++ src/Main/FilesMenus.java
@@ -143,7 +143,7 @@ public class FilesMenus {
             System.out.println("5. Print Video files with duration more than 10 seconds /10,000 mls/.");
             System.out.println("6. Print all Video files sorted by their contents length.");
             System.out.println("7. Play selected Video file from the Video files.");
-            System.out.println("8. Exit the Audio SubMenu");
+            System.out.println("8. Exit the Video SubMenu");
             int command = scan.nextInt();
             switch (command) {
                 case 1 -> VideoService.createVideoFile();
